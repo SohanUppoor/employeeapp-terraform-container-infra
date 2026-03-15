5696a8bdc1436f444aebddac37d3d06327dc7a76
Sohan Uppoor <sohan88.s.u@gmail.com>
2026-03-15 23:21:34 +0530

    updated infra

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -24,3 +24,29 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   name = "employeeapp-ec2-profile"
   role = aws_iam_role.ec2_role.name
 }
+
+# ECS Task Execution Role
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+
+# Attach AWS Managed Policy for ECS Tasks
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
